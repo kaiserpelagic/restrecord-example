27338d18c318597a9c25a5211341053ae69d5ebf
greg <greg.flanagan@locusenergy.com>
2012-11-04 09:00:31 -0800

    adding Search domain object

---
+++ src/main/scala/code/model/Search.scala
@@ -0,0 +1,30 @@
+package code.model
+
+import net.liftweb.record.field._
+import net.liftweb.couchdb._
+import net.liftweb.http.js._ 
+import net.liftweb.json._
+import net.liftweb.common._
+
+import code.lib._
+
+class SearchResult extends JSONRecord[SearchResult] {
+  def meta = SearchResult
+
+  object text extends OptionalStringField(this, Empty)
+}
+
+object SearchResult extends SearchResult with JSONMetaRecord[SearchResult] {
+  override def ignoreExtraJSONFields: Boolean = true
+  override def needAllJSONFields: Boolean = false 
+}
+
+class Search extends RestRecord[Search] {
+  def meta = Search
+
+  override val uri = "search.json" :: Nil
+  
+  object results extends JSONSubRecordArrayField(this, SearchResult)
+}
+
+object Search extends Search with RestMetaRecord[Search] { }
