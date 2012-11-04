e480d8ce298b7c0b3d954a4ad86616fd4648191e
greg <greg.flanagan@locusenergy.com>
2012-11-04 08:58:46 -0800

    adding Twitter domain class

---
+++ src/main/scala/code/snippet/Twitter.scala
@@ -0,0 +1,18 @@
+package code.snippet
+
+import code.model._
+import net.liftweb._
+import common._
+import http._
+import js.JsCmds._
+import util._
+import Helpers._
+import scala.xml._
+
+object Twitter {
+
+  def render = {
+    val r = Search.find(("q", "lift framework")).get.results.is
+    "li *" #> r.map(t => "@text *" #> Text(t.text.valueBox openOr "")) 
+  }
+}
