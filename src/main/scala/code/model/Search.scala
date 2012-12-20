65a54a4a4a059efead1d6c7e37aaafde4b8dd7d6
greg <greg.flanagan@locusenergy.com>
2012-12-20 13:34:52 -0800

    adding status model

--- src/main/scala/code/model/Search.scala
+++ src/main/scala/code/model/Search.scala
@@ -7,23 +7,33 @@ import net.liftweb.common._
 
 import net.liftmodules.restrecord._
 
-class Statuses extends JSONRecord[Statuses] {
-  def meta = Statuses
-
-  object text extends OptionalStringField(this, Empty)
-}
-
-object Statuses extends Statuses with JSONMetaRecord[Statuses] {
-  override def ignoreExtraJSONFields: Boolean = true
-  override def needAllJSONFields: Boolean = false 
-}
-
 class Search extends RestRecord[Search] {
   def meta = Search
 
+  // defines the search resource endpoint
   override val uri = "search" :: "tweets.json" :: Nil
-  
+
   object statuses extends JSONSubRecordArrayField(this, Statuses)
 }
 
 object Search extends Search with RestMetaRecord[Search] { }
+
+class Statuses extends RestRecord[Statuses] {
+  def meta = Statuses
+
+  override val uri = "statuses" :: "show" :: Nil
+
+  // Defines the id in the resource path.
+  // This will be used on Save and Deletes if the Box is Full
+  // Twitter requires ".json" after the id even though they only respond with json !!!
+  override def idPk = Full((id_str.is + ".json"))
+
+  object id_str extends StringField(this, "")
+  object text extends OptionalStringField(this, Empty)
+}
+
+object Statuses extends Statuses with RestMetaRecord[Statuses] {
+  // allows for flexible parsing of the json
+  override def ignoreExtraJSONFields: Boolean = true
+  override def needAllJSONFields: Boolean = false 
+}
